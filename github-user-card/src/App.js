db204ea91a30edd27cd34ddadf7a8cd4e5166491
teddyhn <tngo97@gmail.com>
2019-09-03 15:43:22 -0700

    Basic styling complete

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -31,15 +31,17 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <h1>Github User: {this.state.user.login}'s Card</h1>
-        <img src={this.state.user.avatar_url} />
-        <h2>{this.state.user.name}</h2>
-        <h3>{this.state.user.bio}</h3>
-        <h3>{this.state.user.location}</h3>
-        <h4>Followers:</h4>
-        {this.state.followers.map(follower => (
-            <p>{follower.login}</p>
-          ))}
+        <div className="card">
+          <h1>Github User: {this.state.user.login}'s Card</h1>
+          <img src={this.state.user.avatar_url} />
+          <h2>{this.state.user.name}</h2>
+          <h3>{this.state.user.bio}</h3>
+          <h3>{this.state.user.location}</h3>
+          <h4>Followers:</h4>
+          {this.state.followers.map(follower => (
+              <p>{follower.login}</p>
+            ))}
+        </div>
       </div>
     )
   }
